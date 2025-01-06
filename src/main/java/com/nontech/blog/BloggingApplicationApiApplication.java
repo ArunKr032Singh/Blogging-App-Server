5c6862d7bea85cd0fae07d782e8a7116269cf581
ArunKr032Singh <arunkrasingh@gmail.com>
2025-01-06 11:57:03 +0530

    initial commit database configuration

---
+++ src/main/java/com/nontech/blog/BloggingApplicationApiApplication.java
@@ -0,0 +1,13 @@
+package com.nontech.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingApplicationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingApplicationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nontech/blog/BloggingApplicationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nontech.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingApplicationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
